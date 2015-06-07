cb18aa6d387d567ae1a9d5ac3f9d2197862b2400
Justin Carruthers <Jtcruthers@gmail.com>
2015-06-07 00:10:43 -0400

    Test Commit.

--- src/com/lockness/recipes/Recipe.java
+++ src/com/lockness/recipes/Recipe.java
@@ -1,12 +1,9 @@
 package com.lockness.recipes;
 import java.sql.Array;
+
 public class Recipe {
 	
 	public String name;
 	public Array ingredients;
-	//Add ingredients. 
-	
-	
-	
-	//TEST comment. 
+
 }
