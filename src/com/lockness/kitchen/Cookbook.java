1e59a51c3a285605ed70e24036484d576b59e632
Ryan Tomlinson <ryantomlinson95@gmail.com>
2015-06-07 04:31:57 -0400

    Added methods to cookbook with complete javadocs

--- src/com/lockness/kitchen/Cookbook.java
+++ src/com/lockness/kitchen/Cookbook.java
@@ -1,9 +1,29 @@
 package com.lockness.kitchen;
 
+import java.util.TreeMap;
+
 public class Cookbook {
 
-	public Cookbook() {
-		
+	/**
+	 * An alphabetically-ordered map or recipes.
+	 */
+	TreeMap<String, Recipe> recipeList;
+
+	/**
+	 * No-argument constructor.
+	 */
+	public Cookbook(){
+		this.recipeList = null;
 	}
 	
+	/**
+	 * Adds a recipe to the cookbook.
+	 * @param name
+	 * 	The name of the recipe
+	 * @param recipe
+	 * 	The complete recipe itself
+	 */
+	public void addRecipe(String name, Recipe recipe){
+		this.recipeList.put(name, recipe);
+	}
 }
