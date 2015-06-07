b3fe73f2260a1aa690ca603d6371b0abd0ae9226
Justin Carruthers <Jtcruthers@gmail.com>
2015-06-07 17:26:00 -0400

    Working .ing file parser

--- src/com/lockness/kitchen/Test.java
+++ src/com/lockness/kitchen/Test.java
@@ -20,7 +20,7 @@ public class Test {
 		int servingSize = -1, time[] = new int[3];
 		TreeMap<String, Ingredient> ingredients = new TreeMap<String, Ingredient>();
 
-		File file = new File("Recipe/test.xml");
+		File file = new File("Recipe/test.ing");
 		Scanner input;
 		try {
 			input = new Scanner(file);
@@ -30,12 +30,14 @@ public class Test {
 			switch (currentLine.charAt(0)) {
 			case '0':
 				name = currentLine.substring(2);
+				break;
 			case '1':
 				description = currentLine.substring(2);
+				break;
 			case '2':
 				currentLine = currentLine.substring(2);
-				System.out.println(currentLine);
 				servingSize = Integer.parseInt(currentLine);
+				break;
 			case '3':
 				currentLine = currentLine.substring(2);
 				int nextSpace = currentLine.indexOf(' ');
@@ -45,15 +47,19 @@ public class Test {
 				time[1] = Integer.parseInt(currentLine.substring(0, nextSpace));
 				currentLine = currentLine.substring(nextSpace + 1);
 				time[2] = Integer.parseInt(currentLine);
+				break;
 			case '4':
-				System.out.println("CASE 4");
+				//System.out.println("CASE 4");
+				break;
+			default:
+				System.err.println("ERROR: Error parsing .ing file. Line not labeled correctly.!");
 			}
 		}
 
 		System.out.println("NAME: " + name);
 		System.out.println("DESCRIP: " + description);
 		System.out.println("SERVING: " + servingSize);
-		System.out.println("TIME: " + time.toString());
+		System.out.println("TIME: " + time[0] + " " + time[1] + " " + time[2]);
 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
