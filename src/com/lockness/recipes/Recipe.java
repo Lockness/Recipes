eacc53d138fcb2a63fef6660cd124eb3a2b1f2b1
Ryan Tomlinson <ryantomlinson95@gmail.com>
2015-06-07 00:08:30 -0400

    Added ingredients

--- src/com/lockness/recipes/Recipe.java
+++ src/com/lockness/recipes/Recipe.java
@@ -1,9 +1,9 @@
 package com.lockness.recipes;
-
+import java.sql.Array;
 public class Recipe {
 	
-	public String name2;
-	
+	public String name;
+	public Array ingredients;
 	//Add ingredients. 
 	
 	
